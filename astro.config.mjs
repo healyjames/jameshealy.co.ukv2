cadef4add90f1f02c76cad631407ea8ab635d077
James <healyjames97@gmail.com>
2024-08-01 11:14:26 +0100

    chore: add node config for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   devToolbar: {
     enabled: false
-  }
+  },
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
