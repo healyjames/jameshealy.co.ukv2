8c60887add2d67ac528cb95e7634480e6b2ced5f
James <healyjames97@gmail.com>
2025-01-21 22:11:10 +0000

    test: add test description

--- cypress/e2e/index.cy.ts
+++ cypress/e2e/index.cy.ts
@@ -1,4 +1,6 @@
-it('titles are correct', () => {
-    const page = cy.visit('http://localhost:4321/persimmon-demo');
-    page.get('h1').should('have.text', 'Find your new Persimmon home');
+describe('Persimmon search tests', () => {
+    it('titles are correct', () => {
+        const page = cy.visit('http://localhost:4321/persimmon-demo');
+        page.get('h1').should('have.text', 'Find your new Persimmon home');
+    });
 });
