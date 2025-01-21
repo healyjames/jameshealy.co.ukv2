88fc31209c5ed90068d6f9c650b44df7915d9311
James <healyjames97@gmail.com>
2025-01-21 20:09:30 +0000

    feat: add onclick to results to be taken to results page

--- src/components/SearchDropdown.tsx
+++ src/components/SearchDropdown.tsx
@@ -5,11 +5,15 @@ interface SearchDropdownProps {
     locations: Location[];
 }
 
+const handleOnClick = ( location: Location ) => {
+    window.location.href = `https://www.persimmonhomes.com/find-your-new-home/search-results?placeId=${location.PlaceId}&Location=${location.Name}`;
+}
+
 export const SearchDropdown = ({ locations }: SearchDropdownProps) => {
     return (
         <div>
             {locations.map((location, index) => (
-                <div key={index}>
+                <div key={index} onClick={() => handleOnClick(location)}>
                     {location.Name}
                 </div>
             ))}
