dbbd2782b17382e8a4a28bf2c26db772c673eb4d
James <healyjames97@gmail.com>
2025-01-21 22:24:28 +0000

    test: ensure component renders

--- cypress/e2e/index.cy.ts
+++ cypress/e2e/index.cy.ts
@@ -3,4 +3,9 @@ describe('Persimmon search tests', () => {
         const page = cy.visit('http://localhost:4321/persimmon-demo');
         page.get('h1').should('have.text', 'Find your new Persimmon home');
     });
+
+    it('search component rendered', () => {
+        const page = cy.visit('http://localhost:4321/persimmon-demo');
+        page.get('[data-testid="search-bar"]').should('exist');
+    })
 });
