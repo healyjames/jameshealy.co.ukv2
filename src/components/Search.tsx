1b9dcb8b9c3ffb71d85a7329ed436a5cb7131486
James <healyjames97@gmail.com>
2025-01-21 16:52:15 +0000

    feat: fetch data from api

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -4,12 +4,25 @@ export const SearchBar = () => {
 
     const [inputVal, setInputVal] = useState<string>('')
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleInputChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
         const value = event.target.value;
         setInputVal(value);
 
         if (value.length >= 3) {
-            // fetch from api if at least 3 characters have been implemented
+            try {
+                const response = await fetch(`https://www.persimmonhomes.com/umbraco/Api/LocationsSearch/GetPlaces?searchKeyword=${value}&sessionToken=34bxqruj25jx74wbrqweih`); // hardcoded token purely for a working example. 
+
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+
+                const data = await response.json()
+
+                console.log(data)
+
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
         }
     }
 
